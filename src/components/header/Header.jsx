791ec446e4f3fb7aac28e77541a32ceb1fc141ca
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-07-27 20:46:40 +0500

    header and mini header done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import {
+  TbMapPin,
+  TbPhone,
+  TbMailFilled,
+  TbPhoneFilled,
+  TbChevronDown,
+} from "react-icons/tb";
+import { RiAlignLeft } from "react-icons/ri";
+import { Button, Modal } from "antd";
+
+import logo from "../../assets/logo.png";
+import uz from "../../assets/uzFlag.png";
+import ru from "../../assets/ruFlag.png";
+
+import style from "./Header.module.css";
+
+const Header = () => {
+  const [modal2Open, setModal2Open] = useState(false);
+  const [sm_sidebar, setSm_sidebar] = useState(false);
+
+  return (
+    <div className={style.body}>
+      <div className={style.block_1}>
+        <img src={logo} alt="" />
+        <h1>mediamag.uz </h1>
+        <p>Elektronika va maishiy texnika onlayn-do'koni.</p>
+      </div>
+      <div className={style.block_2}>
+        <span>
+          <TbMapPin />
+        </span>
+        <p>Onlayn do'kon</p>
+      </div>
+      <div className={style.block_3}>
+        <span>
+          <TbPhone />
+        </span>
+        <div>
+          <h1> +998 90 174 76 74</h1>
+          <p>Пн - Пт 10:00 - 18:00</p>
+        </div>
+        <Button type="primary" onClick={() => setModal2Open(true)}>
+          <TbMailFilled />
+        </Button>
+        <Modal
+          title="Bizga yozing"
+          centered
+          open={modal2Open}
+          onOk={() => setModal2Open(false)}
+          onCancel={() => setModal2Open(false)}
+          width={400}
+        >
+          <div className={style.model}>
+            <input type="text" placeholder="Ism" />
+            <input type="text" placeholder="Telefon raqam*" />
+            <textarea name="" id="" placeholder="Izoh" rows={7} />
+          </div>
+          <button className={style.btn}>Jo'natish</button>
+        </Modal>
+      </div>
+      <div className={style.block_md}>
+        <div className={style.md_padding}>
+          <Button
+            className={style.md_btn}
+            type="primary"
+            onClick={() => setModal2Open(true)}
+          >
+            <TbPhoneFilled /> Aloqalar <TbChevronDown />
+          </Button>
+        </div>
+        <Modal
+          title="Bizga yozing"
+          centered
+          open={modal2Open}
+          onOk={() => setModal2Open(false)}
+          onCancel={() => setModal2Open(false)}
+          width={400}
+        >
+          <div className={style.model}>
+            <input type="text" placeholder="Ism" />
+            <input type="text" placeholder="Telefon raqam*" />
+            <textarea name="" id="" placeholder="Izoh" rows={7} />
+          </div>
+          <button className={style.btn}>Jo'natish</button>
+        </Modal>
+        <div className={style.lang}>
+          <img src={ru} alt="" />
+          <img src={uz} alt="" />
+        </div>
+      </div>
+      <div className={style.block_sm}>
+        <RiAlignLeft />
+        <div style={{ cursor: "pointer" }}>
+          <img src={logo} alt="" />
+          <p>mediamag.uz</p>
+        </div>
+        <div className={style.lang}>
+          <img src={ru} alt="" />
+          <img src={uz} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/miniHeader/MiniHeader.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Button, Dropdown, Modal, Space } from "antd";
+import { IoPersonCircleOutline } from "react-icons/io5";
+
+import uz from '../../assets/uzFlag.png'
+import ru from '../../assets/ruFlag.png'
+
+import style from "./MiniHeader.module.css";
+
+const MiniHeader = () => {
+  const [modal2Open, setModal2Open] = useState(false);
+  const items = [
+    {
+      key: "1",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Maxfiylik Siyosati
+        </a>
+      ),
+    },
+  ];
+
+  return (
+    <div className={style.body}>
+      <div className={style.block_1}>
+        <Space direction="vertical">
+          <Space wrap>
+            <Dropdown menu={{ items }} placement="bottomLeft" arrow>
+              <Button>Biz haqimizda</Button>
+            </Dropdown>
+            <Dropdown menu={{ items }} placement="bottomLeft" arrow>
+              <Button>To'lov usullari</Button>
+            </Dropdown>
+          </Space>
+        </Space>
+
+        <a href="/">Muddatli to'lov islomda</a>
+        <a href="/">Tekshirish IMEI</a>
+        <a href="/">To'lovlar uchun kafolat</a>
+        <a href="/">Bepul yuk</a>
+      </div>
+      <div className={style.block_2}>
+        <Button type="primary" onClick={() => setModal2Open(true)}>
+        <IoPersonCircleOutline />
+        Kirish / Ro'yxatdan o'tish
+        </Button>
+        <Modal
+          title={ ` Kabinet`}
+          centered
+          open={modal2Open}
+          onOk={() => setModal2Open(false)}
+          onCancel={() => setModal2Open(false)}
+        >
+           <div className={style.model}>
+
+<input type="text" placeholder="Login" />
+<input type="text" placeholder="Parol*" />
+  </div>
+  <button className={style.btn}>Kirish</button>
+        </Modal>
+        <span>
+          <img src={uz} alt="" />
+          <img src={ru} alt="" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default MiniHeader;
